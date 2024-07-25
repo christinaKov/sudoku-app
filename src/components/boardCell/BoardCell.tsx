58b882d1d44d01ec6b2a20dd472715727e269ba8
christinaKov <kov.christina.kov@gmail.com>
2024-07-25 23:24:18 +0300

    added validation check

--- src/components/boardCell/BoardCell.tsx
+++ src/components/boardCell/BoardCell.tsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
 import { Cell } from "../../app/sudokuSlice";
 
 const BoardCell = ({ cell }: { cell: Cell }) => {
-	return <div className="sudoku-cell">{cell.value}</div>;
+	const [cellValue, setCellValue] = useState(
+		cell.value === "-" ? "" : cell.value
+	);
+	const [isCorrect, setIsCorrect] = useState<boolean>(true);
+
+	const handleCellChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const nativeEvent = e.nativeEvent as InputEvent;
+		const eventData = nativeEvent.data || "";
+
+		if (eventData.match(/[0-9]+/) && eventData !== "") {
+			setCellValue(eventData);
+			setIsCorrect(cell.correctValue === eventData);
+		}
+	};
+
+	return (
+		<input
+			disabled={cell.value !== "-"}
+			onInput={handleCellChange}
+			className={
+				"sudoku-cell " +
+				(cell.correctValue === cell.value ? "" : "is--empty ") +
+				(isCorrect ? "" : "is--incorrect ") +
+				(isCorrect ? "is--correct" : "")
+			}
+			value={cellValue}
+		/>
+	);
 };
 
 export default BoardCell;
