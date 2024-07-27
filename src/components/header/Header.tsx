7625a09933fb7d29c84fcc389cd063ff0825c68a
christinaKov <kov.christina.kov@gmail.com>
2024-07-27 15:59:34 +0300

    restart game added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,19 @@
 // styles
 import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
+
+// redux
+import { useDispatch } from "react-redux";
+import { setSudokuGen } from "../../app/sudokuSlice";
 
 const Header = () => {
+	const dispatch = useDispatch();
+
+	const handleReset = () => {
+		dispatch(setSudokuGen());
+	};
+
 	return (
 		<header className="header">
 			<h1 className="main-title">Sudoku</h1>
@@ -10,15 +22,22 @@ const Header = () => {
 					<li>
 						<label htmlFor="">
 							Difficulty:
-							<select name="difficulty" id="difficulty">
-								<option selected value="">
-									Random
-								</option>
-								<option value="">Easy</option>
-								<option value="">Medium</option>
-								<option value="">Hard</option>
+							<select defaultValue={"random"} name="difficulty" id="difficulty">
+								<option value="random">Random</option>
+								<option value="easy">Easy</option>
+								<option value="medium">Medium</option>
+								<option value="hard">Hard</option>
 							</select>
 						</label>
+					</li>
+					<li>
+						<div onClick={handleReset} className="reset-game_container">
+							<div className="reset-game_tooltip">Start Again</div>
+							<FontAwesomeIcon
+								icon={faArrowsRotate}
+								className="reset-game_btn"
+							/>
+						</div>
 					</li>
 				</ul>
 			</nav>
